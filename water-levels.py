a55ab05658272476d6edba2d5d3b83af6f30d8cf
Tom Lee <tom_t_lee@yahoo.com>
2015-10-01 17:55:10 +0000

    Add functionality for whole state

--- water-levels.py
+++ water-levels.py
@@ -1,25 +1,33 @@
 import urllib2
+import urllib
 import json
 
-def get_data_from_usgs():
-    url = "http://waterservices.usgs.gov/nwis/iv/?site=01408205&startDT=2015-09-30&format=json&modifiedSince=P2D&parameterCd=00065"
+def get_data_from_usgs(state_code):
+    query_args = {"stateCd": state_code, 
+                  "startDT":"2015-09-30", 
+                  "format": "json", 
+                  "modifiedSince": "P2D", 
+                  "parameterCd": "00065"}
+                  
+    encoded_args = urllib.urlencode(query_args)
+    url = "http://waterservices.usgs.gov/nwis/iv/?" + encoded_args
     response = urllib2.urlopen(url)
     data = json.load(response)
     return data
 
 def gauge_info(data):
     gauge_info = {}
-    gauge_info["name"] = data["value"]["timeSeries"][0]["sourceInfo"]["siteName"]
-    gauge_info ["site_code"] = data["value"]["timeSeries"][0]["sourceInfo"]["siteCode"][0]["value"]
-    gauge_info["lat"] = data["value"]["timeSeries"][0]["sourceInfo"]["geoLocation"]["geogLocation"]["latitude"]
-    gauge_info["lng"] = data["value"]["timeSeries"][0]["sourceInfo"]["geoLocation"]["geogLocation"]["longitude"]
-    gauge_info["h_datum"] = data["value"]["timeSeries"][0]["sourceInfo"]["geoLocation"]["geogLocation"]["srs"]
+    gauge_info["name"] = data["siteName"]
+    gauge_info ["site_code"] = data["siteCode"][0]["value"]
+    gauge_info["lat"] = data["geoLocation"]["geogLocation"]["latitude"]
+    gauge_info["lng"] = data["geoLocation"]["geogLocation"]["longitude"]
+    gauge_info["h_datum"] = data["geoLocation"]["geogLocation"]["srs"]
     return gauge_info
 
 def max_gauge_reading(data):
     readings = {}
     
-    for reading in data["value"]["timeSeries"][0]["values"][0]["value"]:
+    for reading in data[0]["value"]:
         date_time = reading["dateTime"]
         gauge_height = reading["value"]
         readings[gauge_height] = date_time
@@ -29,9 +37,13 @@ def max_gauge_reading(data):
     return {"peak_gauge_height": max_gauge_height, "peak_time": max_gauge_time}
 
 
+state_code = raw_input("Enter state (eg: NJ): ")
+data = get_data_from_usgs(state_code)
 
-data = get_data_from_usgs()
-gauge_data = gauge_info(data)
-max_reading = max_gauge_reading(data)
-gauge_data.update(max_reading)
-print gauge_data
+for gauge in data["value"]["timeSeries"]:
+    gauge_info_data = gauge["sourceInfo"]
+    gauge_reading_data = gauge["values"]
+    gauge_summary_data = gauge_info(gauge_info_data)
+    max_reading = max_gauge_reading(gauge_reading_data)
+    gauge_summary_data.update(max_reading)
+    print gauge_summary_data
